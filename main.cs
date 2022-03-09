08fc2c05f247ff3440196b1ae22300166ab909e0
CristianoSellitto <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-09 13:27:20 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,9 +2,11 @@
 
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    Console.WriteLine("\nFinished.");
+class Program 
+{
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine ("Hello World");
+        Console.WriteLine("\nFinished.");
+    }
   }
-}
